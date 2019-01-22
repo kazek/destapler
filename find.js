388315699fc197331128a20fc96e4130c830c4ae
Kazimierz Zagroba <kazimierz.zagroba@gmail.com>
2019-01-22 20:50:40 +0100

    added extension check to find

--- find.js
+++ find.js
@@ -3,21 +3,22 @@ const fs = require('fs');
 const findInFile = (pattern, path) => fs.readFileSync(path, 'utf8')
   .split('\n')
   .reduce((results, line) => {
-    //console.log('line', line)
     if (~line.search(pattern)) {
       results.push(line);
     }
     return results
   }, []);
 
- const findInDirectory = (pattern, path) => {
+ const findInDirectory = (pattern, path, extensions = ['js', 'ts']) => {
   let results = [];
   fs.readdirSync(path).forEach(file => {
     if (file !== 'node_modules') {
       if (fs.statSync(file).isDirectory()) {
         results = [...results, ...findInDirectory(pattern, file)];
       } else {
-        results = [...results, ...findInFile(pattern, file)];
+        if(~extensions.indexOf(file.split('.').pop())) {
+          results = [...results, ...findInFile(pattern, file)];
+        }
       }
     }
   });
