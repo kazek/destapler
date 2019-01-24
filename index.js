c71aa3e5a1afa7ae72a1efe86107c50be2bbc2eb
Kazimierz Zagroba <kazimierz.zagroba@gmail.com>
2019-01-24 23:14:46 +0100

    added weight and emotion calculations

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const calculateModuleSize = require('./moduleSize');
 
 const findInDirectory = require('./find');
 
+const setEmotion = (weight) => {
+  if (weight > 0.8) return '😱';
+  if (weight > 0.7) return '😨';
+  if (weight > 0.6) return '😬';
+  if (weight > 0.5) return '😕';
+  if (weight > 0.3) return '😐';
+  if (weight > 0.1) return '🙂';
+  return '😊'
+}
 
 const countImports = () => findInDirectory(/require\([\`\'\"].[^\.\/]..*\)/i, process.cwd())
   .map(l => {
@@ -29,10 +38,25 @@ module.exports = () => {
   )
     .then((treeWithSizes) => {
       const numberOfImports = countImports();
-      console.log(treeWithSizes.map(a => ({
-        name: a.name,
-        size: a.size,
-        imports: numberOfImports[a.name] || 0
-      })));
+      const maxSize = Math.max(...treeWithSizes.map(a => a.size));
+      let result = treeWithSizes.map(a => {
+        const imports = numberOfImports[a.name] || 0;
+        const weight = (imports ? a.size / imports : a.size * 2) / maxSize;
+        const emotion = setEmotion(weight)
+
+        return {
+          name: a.name,
+          size: a.size,
+          imports,
+          weight,
+          emotion
+        }
+      });
+      
+      result.sort((a, b) => b.weight - a.weight);
+      
+      console.log(
+        result.map(a => ({name: a.name, emotion: a.emotion}))
+      );
   });
 }
