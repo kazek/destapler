ad95441e759b2c6011b19929d2bd6b8da35dd0b3
Kazimierz Zagroba <kazimierz.zagroba@gmail.com>
2019-01-31 23:55:02 +0100

    added generateBar function

--- index.js
+++ index.js
@@ -8,14 +8,30 @@ const findInDirectory = require('./find');
 
 const package = require('./package.json');
 
-const setEmotion = (weight) => {
+const setEmotion = weight => {
   if (weight > 0.8) return '😱';
-  if (weight > 0.7) return '😨';
-  if (weight > 0.6) return '😬';
-  if (weight > 0.5) return '😕';
-  if (weight > 0.3) return '😐';
+  if (weight > 0.5) return '😨';
+  if (weight > 0.4) return '😬';
+  if (weight > 0.3) return '😕';
+  if (weight > 0.2) return '😐';
   if (weight > 0.1) return '🙂';
   return '😊';
+}
+
+const generateBar = dep => {
+  const maxWidth = parseInt(process.stdout.columns/1.5);
+  const fullLength = parseInt(dep.weight * maxWidth);
+  let barSize = fullLength - dep.name.length;
+  barSize = barSize > 0 ? barSize : 0;
+  const CLR = '\x1b[0m';
+
+  const bar = `${dep.name}${[...new Array(barSize)].join(' ')}${[...new Array(maxWidth)].join(' ')}|`.substring(0, maxWidth);
+
+  console.log(
+    dep.emotion + ' \x1b[30m\x1b[41m\x1b[4m'
+    + [bar.slice(0, fullLength), CLR, bar.slice(fullLength)].join('')
+    +`${CLR} ` + dep.imports + ' '
+  )
 }
 
 const countImports = () => findInDirectory(/require\([\`\'\"].[^\.\/]..*\)/i, process.cwd())
@@ -72,7 +88,7 @@ module.exports = () => {
     let result = treeWithSizes.map(a => {
       const imports = numberOfImports[a.name] || 0;
       const size = a[sizeType];
-      const weight = (imports ? size / imports : size * 2) / maxSize;
+      const weight = imports ? ((size / imports) / maxSize) : 1;
       const emotion = setEmotion(weight)
 
       return {
@@ -87,9 +103,7 @@ module.exports = () => {
     });
     
     result.sort((a, b) => b.weight - a.weight);
+    result.forEach(generateBar);
     
-    console.log(
-      result.map(a => ({emotion: a.emotion, size: a[sizeType], name: a.name}))
-    );
   });
 }
